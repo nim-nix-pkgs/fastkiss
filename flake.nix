657e1c05846245f0ca21d352e673b5957f57a745
hugosenari <hugosenari@gmail.com>
2022-03-12 00:08:42 -0300

    chore: re index fastkiss

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = ''FastCGI Web Framework for Nim.'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs."fastkiss-master".url = "path:./master";
+  inputs."fastkiss-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."fastkiss-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib"];
+  in lib.mkProjectOutput {
+    inherit self nixpkgs;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+    refs = builtins.removeAttrs inputs args;
+  };
+}
---
+++ master/flake.nix
@@ -0,0 +1,34 @@
+{
+  description = ''FastCGI Web Framework for Nim.'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-fastkiss-master.flake = false;
+  inputs.src-fastkiss-master.owner = "mrhdias";
+  inputs.src-fastkiss-master.ref   = "refs/heads/master";
+  inputs.src-fastkiss-master.repo  = "fastkiss";
+  inputs.src-fastkiss-master.type  = "github";
+  
+  inputs."regex".dir   = "nimpkgs/r/regex";
+  inputs."regex".owner = "riinr";
+  inputs."regex".ref   = "flake-pinning";
+  inputs."regex".repo  = "flake-nimble";
+  inputs."regex".type  = "github";
+  inputs."regex".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."regex".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-FastKiss-master"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-FastKiss-master";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
